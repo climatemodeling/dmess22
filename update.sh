14e67fc1de4639abbe968df3c9df8285cea9a310
Sarat Sreepathi <sarat@ornl.gov>
2022-04-21 13:32:29 -0400

    Initial text

---
+++ update.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+# This website just has one html, css, img and js files.
+wget https://climatemodeling.github.io/dmess22  -O index.html
+wget https://climatemodeling.github.io/dmess22/combo.css -O combo.css
+wget https://climatemodeling.github.io/dmess22/site.js -O site.js
+
+
